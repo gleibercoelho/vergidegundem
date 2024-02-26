0f77a2a16882e82e941691dda207b88e56921ff0
GFC <gleiberfelipe@hotmail.com>
2024-02-26 00:32:10 -0300

    final.7

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
           <CardDemo style={{ backgroundColor: "rgb(46,46,56)" }} />
           <SheetDemo />
         </div>
-        <div className="flex flex-row flex-wrap justify-around mt-7">
+        <div className="flex flex-row flex-wrap justify-around mt-7" style={{ backgroundColor: "rgb(0,0,0)" }}>
         <ComboboxDemo/>
         <ComboboxDemoSnd/>
         <ComboboxDemoTrd/>
